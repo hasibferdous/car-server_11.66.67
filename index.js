23eba9733e5d0a34a479e7e32a48a55498af96ca
hasibferdous <hasibferdous.ict@gmail.com>
2022-11-07 02:32:54 +0600

    update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('smartCar').collection('services');
-        //const orderCollection = client.db('smartCar').collection('orders');
+        const orderCollection = client.db('smartCar').collection('orders');
 
         app.get('/services', async (req, res) => {
             const query = {}
@@ -35,46 +35,46 @@ async function run() {
          });
 
 
-        // // orders api
-        // app.get('/orders', async (req, res) => {
-        //     let query = {};
+        // orders api
+        app.get('/orders', async (req, res) => {
+           let query = {};
 
-        //     if (req.query.email) {
-        //         query = {
-        //             email: req.query.email
-        //         }
-        //     }
+             if (req.query.email) {
+                 query = {
+                     email: req.query.email
+                 }
+             }
 
-        //     const cursor = orderCollection.find(query);
-        //     const orders = await cursor.toArray();
-        //     res.send(orders);
-        // });
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+         });
 
-        // app.post('/orders', async (req, res) => {
-        //     const order = req.body;
-        //     const result = await orderCollection.insertOne(order);
-        //     res.send(result);
-        // });
+        app.post('/orders', async (req, res) => {
+             const order = req.body;
+             const result = await orderCollection.insertOne(order);
+             res.send(result);
+         });
 
-        // app.patch('/orders/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const status = req.body.status
-        //     const query = { _id: ObjectId(id) }
-        //     const updatedDoc = {
-        //         $set:{
-        //             status: status
-        //         }
-        //     }
-        //     const result = await orderCollection.updateOne(query, updatedDoc);
-        //     res.send(result);
-        // })
+         app.patch('/orders/:id', async (req, res) => {
+             const id = req.params.id;
+             const status = req.body.status
+             const query = { _id: ObjectId(id) }
+             const updatedDoc = {
+                 $set:{
+                     status: status
+                 }
+             }
+             const result = await orderCollection.updateOne(query, updatedDoc);
+             res.send(result);
+         })
 
-        // app.delete('/orders/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await orderCollection.deleteOne(query);
-        //     res.send(result);
-        // })
+         app.delete('/orders/:id', async (req, res) => {
+             const id = req.params.id;
+             const query = { _id: ObjectId(id) };
+             const result = await orderCollection.deleteOne(query);
+             res.send(result);
+         })
 
 
     }
